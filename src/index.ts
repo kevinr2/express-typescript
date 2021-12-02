461507849fe7e4fb76ef3b207d8a3540441059bb
Kevinr2 <kevinreve096@gmail.com>
2021-12-02 10:01:34 -0500

    deploy

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import app from './App'
 import './dataBase'
 
-app.listen(4000, () => {
-    console.log("http://localhost:4000")
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(`puerto:${PORT}`)
 })
