3654770c6274f5fc10591c6eb75c580c38c2e185
Kevinr2 <kevinreve096@gmail.com>
2021-12-02 11:12:05 -0500

    v1

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import app from './App'
 import './dataBase'
 
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`)
